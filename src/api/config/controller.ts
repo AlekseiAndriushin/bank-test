71ed12bc22e016d0abee9938d75866771e5bd363
 Алексей Андрюшин <aleksejandryushin@MacBook-Air-Aleksej.local>
2022-01-27 21:44:59 +0300

    ABT-1 Fix api config

--- src/api/config/controller.ts
+++ src/api/config/controller.ts
@@ -1,39 +1,39 @@
 import axiosBase from './axios.base';
+import { formatRequestConfig } from './formatRequestConfig';
 
 import type { AxiosResponse, AxiosRequestConfig } from 'axios';
 
 interface IProps {
-  requestUrl?: string;
+	requestUrl?: string;
 }
 
 export abstract class Controller {
-  private readonly _requestUrl: IProps['requestUrl'];
+	private readonly _requestUrl: IProps['requestUrl'];
 
-  protected constructor({ requestUrl }: IProps) {
-    this._requestUrl = requestUrl;
-  }
+	protected constructor({ requestUrl }: IProps) {
+		this._requestUrl = requestUrl;
+	}
 
-  protected post<T>(path?: string, data?: unknown, config?: AxiosRequestConfig) {
-    const request = () => {
-      return axiosBase.post<T>(
-        this.processPath(path),
-        data,
-        config
-      );
-    };
+	protected get<T>(path?: string, config?: AxiosRequestConfig) {
+		const request = () => {
+			return axiosBase.get<T>(
+				this.processPath(path),
+				formatRequestConfig(config)
+			);
+		};
 
-    return this.processRequest(request);
-  }
+		return this.processRequest(request);
+	}
 
-  private processRequest<T>(request: () => Promise<AxiosResponse<T>>) {
-    if (!this._requestUrl) {
-      throw new Error('Ожидаемый URL-адрес запроса отсутствует');
-    }
+	private processRequest<T>(request: () => Promise<AxiosResponse<T>>) {
+		if (!this._requestUrl) {
+			throw new Error('Expected request url is absent');
+		}
 
-    return request();
-  }
+		return request();
+	}
 
-  private processPath(path?: string) {
-    return `${this._requestUrl}${path || ''}`;
-  }
+	private processPath(path?: string) {
+		return `${this._requestUrl}${path || ''}`;
+	}
 }
